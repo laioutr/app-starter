1e0e2acd94f9bce43ba54cc62dbfbb742f3bef65
Sebastian Langer <sebastian.langer@laioutr.com>
2025-10-31 15:50:07 +0100

    fix: cleanup playgrounds

--- orchestr-playground/nuxt.config.ts
+++ orchestr-playground/nuxt.config.ts
@@ -1,19 +1,4 @@
 import srcModule from '../src/module';
-import laioutrrc from '../laioutrrc.json';
-
-const appsToUninstall = ['@laioutr-app/ui'];
-
-// Uncomment the below line if you don't want/need Shopify
-// appsToUninstall.push("@laioutr-app/shopify")
-
-const clean_laioutrrc = {
-  ...laioutrrc,
-
-  laioutr: {
-    projectSecretKey: false,
-  },
-  apps: laioutrrc.apps.filter((app) => !appsToUninstall.includes(app.name)),
-};
 
 export default defineNuxtConfig({
   modules: [
@@ -23,10 +8,6 @@ export default defineNuxtConfig({
     '@laioutr-core/orchestr',
     '@laioutr-core/orchestr-devtools',
   ],
-  laioutr: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    laioutrrc: clean_laioutrrc as any,
-  },
   i18n: {
     bundle: {
       optimizeTranslationDirective: false,
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,25 +1,17 @@
-import srcModule from '../src/module';
 import laioutrrc from '../laioutrrc.json';
+import srcModule from '../src/module';
 
-const clean_laioutrrc = {
-  ...laioutrrc,
-
-  laioutr: {
-    projectSecretKey: false,
-  },
-};
+// Disable project secret key for playground
+laioutrrc.laioutr.projectSecretKey = false as any;
 
 export default defineNuxtConfig({
   modules: [
     srcModule,
     '@pinia/nuxt', // Added to show in devtools
     '@laioutr-core/frontend-core',
-    '@laioutr-core/orchestr',
-    '@laioutr-core/orchestr-devtools',
   ],
   laioutr: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    laioutrrc: clean_laioutrrc as any,
+    laioutrrc: laioutrrc as any,
   },
   i18n: {
     bundle: {
@@ -28,9 +20,4 @@ export default defineNuxtConfig({
   },
   devtools: { enabled: true },
   compatibilityDate: '2025-09-11',
-  vite: {
-    optimizeDeps: {
-      include: ['ajv', 'json-source-map', 'natural-compare-lite'],
-    },
-  },
 });
